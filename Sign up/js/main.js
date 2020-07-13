06dfb694f9a824e3f68d56cadb217a53ae481792
Victor <hi4u44r306@gmail.com>
2020-07-13 19:52:40 +0800

    nothing

--- Sign up/js/main.js
+++ Sign up/js/main.js
@@ -68,3 +68,11 @@
     
 
 })(jQuery);
+
+$('#datepicker').datepicker({
+    weekStart: 1,
+    daysOfWeekHighlighted: "6,0",
+    autoclose: true,
+    todayHighlight: true,
+});
+$('#datepicker').datepicker("setDate", new Date());

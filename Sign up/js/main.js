892d02ae9e11552f590be261ede4ca5ba176f9a6
Victor <hi4u44r306@gmail.com>
2020-07-15 14:52:39 +0800

    try to verify username and password

--- Sign up/js/main.js
+++ Sign up/js/main.js
@@ -69,10 +69,3 @@
 
 })(jQuery);
 
-$('#datepicker').datepicker({
-    weekStart: 1,
-    daysOfWeekHighlighted: "6,0",
-    autoclose: true,
-    todayHighlight: true,
-});
-$('#datepicker').datepicker("setDate", new Date());

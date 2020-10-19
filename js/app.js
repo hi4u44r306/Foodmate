e06dc87fa28f790057b20def5acc8229301681ba
Victor <hi4u44r306@gmail.com>
2020-10-19 14:47:44 +0800

    can input image and display

--- js/app.js
+++ js/app.js
@@ -307,3 +307,19 @@ function signOut() {
         })
     });
 }
+
+/*read URL*/
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#p-image')
+                .attr('src', e.target.result)
+                .width(150)
+                .height(200);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}

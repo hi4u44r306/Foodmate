ee0a0fc32c7b23c52bc5fd2a7414ddbd31139d27
Victor <hi4u44r306@gmail.com>
2020-10-26 15:21:55 +0800

    can save image in firebase storage

--- js/app.js
+++ js/app.js
@@ -352,3 +352,50 @@ $("#btn-resetPassword").click(function()
         window.alert("Please write your email first");
     }
 });
+
+/*================================Profile Section======================================*/
+var selectedFile;
+/*hide submit button*/
+$( document ).ready(function(){
+    $("#uploadButton").hide();
+})
+
+/*upload image to firebase*/
+$("#file").on("change", function(event){
+    selectedFile = event.target.files[0];
+    $("#uploadButton").show();
+});
+
+/*Submit function*/
+function uploadFile(){
+    // Create a root reference
+    var filename = selectedFile.name;
+    var storageRef = firebase.storage().ref('/profileImage' + filename);
+    var uploadTask = storageRef.put(selectedFile);  
+    // Register three observers:
+    // 1. 'state_changed' observer, called any time the state changes
+    // 2. Error observer, called on failure
+    // 3. Completion observer, called on successful completion
+    uploadTask.on('state_changed', function(snapshot){
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            break;
+        }
+    }, function(error) {
+        // Handle unsuccessful uploads
+    }, function() {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+        console.log('File available at', downloadURL);
+        });
+    });
+}

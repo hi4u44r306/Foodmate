a38d1c104720fd381aba7bff0d6b4ffc4f1ede9f
victor <hi4u44r306@gmail.com>
2020-11-16 14:28:12 +0800

    create show event information and display member list

---
+++ js/personal-event.js
@@ -0,0 +1,8 @@
+function hideMember(){
+    document.getElementById("infosection").style.display = "block";
+    document.getElementById("membersection").style.display = "none";
+}
+function showMember(){
+    document.getElementById("infosection").style.display = "none"
+    document.getElementById("membersection").style.display = "block"
+}
